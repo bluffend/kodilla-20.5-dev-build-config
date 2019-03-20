9cfef2c0b749d2fe648cdf15fd51cb1be5195072
Slawomir Masluk <slawomirmasluk@gmail.com>
2019-03-20 14:58:49 +0100

    Add all files

---
+++ src/containers/App.js
@@ -0,0 +1,54 @@
+import React from 'react';
+import uuid from 'uuid';
+import style from './App.css';
+import Title from '../components/Title';
+
+class App extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            data: [
+                    {
+                        text: "a",
+                        id: 1234
+                    },
+                    {
+                        text: "b",
+                        id: 5678
+                    },
+                    {
+                        text: "c",
+                        id: 9012
+                    },
+                    {
+                        text: "d",
+                        id: 1256
+                    }
+                ],
+            title: 'Training - webpack loaders'
+        };
+    }
+    addTodo(val) {
+        const todo = {
+            text: val,
+            id: uuid.v4(),
+        };
+        const data = [...this.state.data, todo];
+        this.setState({ data });
+    }
+
+    removeTodo(id) {
+        const remainder = this.state.data.filter(todo => todo.id !== id);
+        this.setState({ data: remainder });
+    }
+
+    render() {
+        return (
+            <div className={style.TodoApp}>
+                <Title title={this.state.title} numberOfTasks={this.state.data.length} />
+            </div>
+        );
+    }
+}
+
+export default App;
